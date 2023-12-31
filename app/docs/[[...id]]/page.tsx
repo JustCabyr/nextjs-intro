af125c2ab30bf17f40b6c6f516619235cb350a60
justcabyr <justcabyr@gmail.com>
2023-12-31 02:25:08 -0330

    added catch all route dynamic routing

---
+++ app/docs/[[...id]]/page.tsx
@@ -0,0 +1,6 @@
+const DocsIdPage = () => {
+  return <div>docs id</div>
+  // <div> docs id {params.id} {params.title} </div>
+}
+
+export default DocsIdPage
--- app/docs/[id]/[title]/page.tsx
+++
@@ -1,9 +0,0 @@
-const DocsTitlePage = ({ params }) => {
-  return (
-    <div>
-      docs title {params.id} {params.title}
-    </div>
-  )
-}
-
-export default DocsTitlePage
