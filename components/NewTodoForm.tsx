2bda6812107d65060f30c3f341b66dc187db2a7d
justcabyr <justcabyr@gmail.com>
2024-01-02 21:02:35 -0330

    added client components

---
+++ components/NewTodoForm.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useState } from "react"
+
+const NewTodoForm = () => {
+    const [state, upDateState] = useState('')
+
+    return <div>
+        <form action="">
+            <input type="text" />
+        </form>
+    </div>
+}
+
+export default NewTodoForm
